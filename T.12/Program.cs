656a1a5f66f9fe3e7bf07fa8421008a66941b92b
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-04-09 15:29:27 +0300

    Updated and T.13 has to be fixed.

---
+++ T.12/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T._12
+{
+    class Dwarf
+    {
+        public Dwarf(string name, string hatColor, int physicsPoints)
+        {
+            this.Name = name;
+            this.HatColor = hatColor;
+            this.PhysicsPoints = physicsPoints;
+        }
+
+        public string Name { get; set; }
+
+        public string HatColor { get; set; }
+
+        public int PhysicsPoints { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Peter <:> Red <:> 2000
+            // Once upon a time
+            List<Dwarf> dwarfs = new List<Dwarf>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] dwarfData = input
+                    .Split("<:>", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = dwarfData[0];
+                string hatColor = dwarfData[1];
+                int physicsPoints = int.Parse(dwarfData[2]);
+
+                Dwarf currDwarf = new Dwarf(name, hatColor, physicsPoints);
+
+                if (dwarfs.Any(d => d.Name == name && d.HatColor == hatColor))
+                {
+                    Dwarf dwarfFromList = dwarfs.Find(d => d.Name == name && d.HatColor == hatColor);
+
+                    if (currDwarf.PhysicsPoints > dwarfFromList.PhysicsPoints)
+                    {
+                        dwarfFromList.PhysicsPoints = physicsPoints;
+                    }
+                }
+                else
+                {
+                    dwarfs.Add(currDwarf);
+                }
+            }
+
+            foreach (Dwarf dwarf in dwarfs.OrderByDescending(d => d.PhysicsPoints))
+            {
+                Console.WriteLine($"({dwarf.HatColor.Trim()}) {dwarf.Name.Trim()} <-> {dwarf.PhysicsPoints}");
+            }
+        }
+    }
+}
---
+++ T.13/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T._13
+{
+    class Dragon
+    {
+        public Dragon(string dragonName, int health, int damage, int armor)
+        {
+            this.DragonName = dragonName;
+            this.Health = health;
+            this.Damage = damage;
+            this.Armor = armor;
+        }
+        public string DragonName { get; set; }
+
+        public int Health { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Armor { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int DefaultHealth = 250;
+            const int DefaultDamage = 45;
+            const int DefaultArmor = 10;
+
+            Dictionary<string, List<Dragon>> typeWithItsDragons = new Dictionary<string, List<Dragon>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                //{type} {name} {damage} {health} {armor}
+                string[] dragonInfo = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string type = dragonInfo[0];
+                string name = dragonInfo[1];
+
+                string testDamage = dragonInfo[2];
+                string testHealth = dragonInfo[3];
+                string testArmor = dragonInfo[4];
+
+                int damage;
+                int health;
+                int armor;
+
+                if (string.IsNullOrEmpty(testDamage))
+                {
+                    damage = DefaultDamage;
+                }
+                else
+                {
+                    damage = int.Parse(testDamage);
+                }
+
+                if (string.IsNullOrEmpty(testHealth))
+                {
+                    health = DefaultHealth;
+                }
+                else
+                {
+                    health = int.Parse(testHealth);
+                }
+
+                if (string.IsNullOrEmpty(testArmor))
+                {
+                    armor = DefaultArmor;
+                }
+                else
+                {
+                    armor = int.Parse(testArmor);
+                }
+
+
+                if (typeWithItsDragons.ContainsKey(type))
+                {
+                    if (typeWithItsDragons[type].Any(x => x.DragonName == name))
+                    {
+                        Dragon currDragon = typeWithItsDragons[type].Find(d => d.DragonName == name);
+
+                        currDragon.Damage = damage;
+                        currDragon.Health = health;
+                        currDragon.Armor = armor;
+                    }
+                }
+                else
+                {
+                    typeWithItsDragons.Add(type, new List<Dragon>{ new (name, health, damage, armor) });
+                }
+            }
+
+            foreach (var kvp in typeWithItsDragons)
+            {
+                Console.WriteLine(kvp.Key);
+
+                foreach (var dragon in kvp.Value)
+                {
+                    Console.WriteLine(dragon.DragonName);
+                }
+            }
+        }
+    }
+}
